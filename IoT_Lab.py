2a24903b64178b2165245944b29cf9632d3a0cd6
KSD MSI PC <nhanksd85@gmail.com>
2021-08-14 18:11:49 +0700

    Init Lab6

--- IoT_Lab.py
+++ IoT_Lab.py
@@ -1,23 +1,27 @@
-import sys
-from Adafruit_IO import MQTTClient
+import serial.tools.list_ports
+import random
+import time
+import  sys
+from  Adafruit_IO import  MQTTClient
 
 AIO_FEED_ID = ""
 AIO_USERNAME = ""
 AIO_KEY = ""
 
-def connected(client):
-    print("Ket noi thanh cong ...")
+def  connected(client):
+    print("Ket noi thanh cong...")
     client.subscribe(AIO_FEED_ID)
 
-def subscribe(client , userdata , mid , granted_qos):
-    print("Subscribe thanh cong ...")
+def  subscribe(client , userdata , mid , granted_qos):
+    print("Subcribe thanh cong...")
 
-def disconnected(client):
-    print("Ngat ket noi ...")
+def  disconnected(client):
+    print("Ngat ket noi...")
     sys.exit (1)
 
-def message(client , feed_id , payload):
+def  message(client , feed_id , payload):
     print("Nhan du lieu: " + payload)
+    ser.write((str(payload) + "#").encode())
 
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
@@ -27,5 +31,44 @@ client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
 
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        if "USB Serial Device" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = (splitPort[0])
+    return commPort
+
+ser = serial.Serial( port=getPort(), baudrate=115200)
+
+mess = ""
+def processData(data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    if splitData[1] == "TEMP":
+        client.publish("bbc-temp", splitData[2])
+
+mess = ""
+def readSerial():
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
+
 while True:
-    pass
+    readSerial()
+    time.sleep(1)
