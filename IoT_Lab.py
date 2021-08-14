65946a21604d8e1e85f136f467a2d96dd0d99b65
KSD MSI PC <nhanksd85@gmail.com>
2021-08-14 18:21:43 +0700

    Init Lab5

--- IoT_Lab.py
+++ IoT_Lab.py
@@ -1,3 +1,4 @@
+import serial.tools.list_ports
 import random
 import time
 import  sys
@@ -20,6 +21,22 @@ def  disconnected(client):
 
 def  message(client , feed_id , payload):
     print("Nhan du lieu: " + payload)
+    ser.write((str(payload) + "#").encode())
+
+
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        if "USB Serial Device" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = (splitPort[0])
+    return commPort
+
+ser = serial.Serial( port=getPort(), baudrate=115200)
 
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
