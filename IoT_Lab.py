4b73921a925366bc1c718e01de0aa005115b9341
KSD MSI PC <nhanksd85@gmail.com>
2021-08-14 18:15:29 +0700

    Init Lab4

--- IoT_Lab.py
+++ IoT_Lab.py
@@ -1,4 +1,3 @@
-import serial.tools.list_ports
 import random
 import time
 import  sys
@@ -13,7 +12,7 @@ def  connected(client):
     client.subscribe(AIO_FEED_ID)
 
 def  subscribe(client , userdata , mid , granted_qos):
-    print("Subcribe thanh cong...")
+    print("Subscribe thanh cong...")
 
 def  disconnected(client):
     print("Ngat ket noi...")
@@ -21,7 +20,6 @@ def  disconnected(client):
 
 def  message(client , feed_id , payload):
     print("Nhan du lieu: " + payload)
-    ser.write((str(payload) + "#").encode())
 
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
@@ -31,44 +29,8 @@ client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
 
-def getPort():
-    ports = serial.tools.list_ports.comports()
-    N = len(ports)
-    commPort = "None"
-    for i in range(0, N):
-        port = ports[i]
-        strPort = str(port)
-        if "USB Serial Device" in strPort:
-            splitPort = strPort.split(" ")
-            commPort = (splitPort[0])
-    return commPort
-
-ser = serial.Serial( port=getPort(), baudrate=115200)
-
-mess = ""
-def processData(data):
-    data = data.replace("!", "")
-    data = data.replace("#", "")
-    splitData = data.split(":")
-    print(splitData)
-    if splitData[1] == "TEMP":
-        client.publish("bbc-temp", splitData[2])
-
-mess = ""
-def readSerial():
-    bytesToRead = ser.inWaiting()
-    if (bytesToRead > 0):
-        global mess
-        mess = mess + ser.read(bytesToRead).decode("UTF-8")
-        while ("#" in mess) and ("!" in mess):
-            start = mess.find("!")
-            end = mess.find("#")
-            processData(mess[start:end + 1])
-            if (end == len(mess)):
-                mess = ""
-            else:
-                mess = mess[end+1:]
-
 while True:
-    readSerial()
-    time.sleep(1)
+    value = random.randint(0, 100)
+    print("Cap nhat:", value)
+    client.publish("bbc-temp", value)
+    time.sleep(30)
