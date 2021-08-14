b312d05812ffb3319130fbde39a817a2f79b4c33
KSD MSI PC <nhanksd85@gmail.com>
2021-08-14 18:01:58 +0700

    no message

---
+++ IoT_Lab.py
@@ -0,0 +1,31 @@
+import sys
+from Adafruit_IO import MQTTClient
+
+AIO_FEED_ID = ""
+AIO_USERNAME = ""
+AIO_KEY = ""
+
+def connected(client):
+    print("Ket noi thanh cong ...")
+    client.subscribe(AIO_FEED_ID)
+
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
+
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit (1)
+
+def message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload)
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect ()
+client.loop_background ()
+
+while True:
+    pass
