08bbec24a3081f7480459794a010bdeef29f9eee
KSD MSI PC <nhanksd85@gmail.com>
2021-12-30 00:39:10 +0700

    Init for Thingsboard

--- IoT_Lab.py
+++ IoT_Lab.py
@@ -1,88 +1,55 @@
-import serial.tools.list_ports
-import random
+print("Xin chào ThingsBoard")
+import paho.mqtt.client as mqttclient
 import time
-import  sys
-from  Adafruit_IO import  MQTTClient
+import json
 
-AIO_FEED_IDS = ["bbc-led", "bbc-pump"]
-
-
-AIO_USERNAME = ""
-AIO_KEY = ""
-
-def  connected(client):
-    print("Ket noi thanh cong...")
-    for feed in AIO_FEED_IDS:
-        client.subscribe(feed)
-
-def  subscribe(client , userdata , mid , granted_qos):
-    print("Subcribe thanh cong...")
-
-def  disconnected(client):
-    print("Ngat ket noi...")
-    sys.exit (1)
-
-def  message(client , feed_id , payload):
-    print("Nhan du lieu: " + payload)
-    if isMicrobitConnected:
-        ser.write((str(payload) + "#").encode())
-
-client = MQTTClient(AIO_USERNAME , AIO_KEY)
-client.on_connect = connected
-client.on_disconnect = disconnected
-client.on_message = message
-client.on_subscribe = subscribe
-client.connect()
-client.loop_background()
-
-def getPort():
-    ports = serial.tools.list_ports.comports()
-    N = len(ports)
-    commPort = "None"
-    for i in range(0, N):
-        port = ports[i]
-        strPort = str(port)
-        if "USB Serial Device" in strPort:
-            splitPort = strPort.split(" ")
-            commPort = (splitPort[0])
-    return commPort
-
-isMicrobitConnected = False
-if getPort() != "None":
-    ser = serial.Serial( port=getPort(), baudrate=115200)
-    isMicrobitConnected = True
+BROKER_ADDRESS = "demo.thingsboard.io"
+PORT = 1883
+THINGS_BOARD_ACCESS_TOKEN = ""
 
 
-def processData(data):
-    data = data.replace("!", "")
-    data = data.replace("#", "")
-    splitData = data.split(":")
-    print(splitData)
+def subscribed(client, userdata, mid, granted_qos):
+    print("Subscribed...")
+
+
+def recv_message(client, userdata, message):
+    print("Received: ", message.payload.decode("utf-8"))
+    temp_data = {'value': True}
     try:
-        if splitData[1] == "TEMP":
-            client.publish("bbc-temp", splitData[2])
-        elif splitData[2] == "HUMI":
-            client.publish("bbc-temp", splitData[3])
+        jsonobj = json.loads(message.payload)
+        if jsonobj['method'] == "setValue":
+            temp_data['value'] = jsonobj['params']
+            client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
     except:
         pass
 
-mess = ""
-def readSerial():
-    bytesToRead = ser.inWaiting()
-    if (bytesToRead > 0):
-        global mess
-        mess = mess + ser.read(bytesToRead).decode("UTF-8")
-        while ("#" in mess) and ("!" in mess):
-            start = mess.find("!")
-            end = mess.find("#")
-            processData(mess[start:end + 1])
-            if (end == len(mess)):
-                mess = ""
-            else:
-                mess = mess[end+1:]
 
+def connected(client, usedata, flags, rc):
+    if rc == 0:
+        print("Thingsboard connected successfully!!")
+        client.subscribe("v1/devices/me/rpc/request/+")
+    else:
+        print("Connection is failed")
+
+
+client = mqttclient.Client("Gateway_Thingsboard")
+client.username_pw_set(THINGS_BOARD_ACCESS_TOKEN)
+
+client.on_connect = connected
+client.connect(BROKER_ADDRESS, 1883)
+client.loop_start()
+
+client.on_subscribe = subscribed
+client.on_message = recv_message
+
+temp = 30
+humi = 50
+light_intesity = 100
+counter = 0
 while True:
-    if isMicrobitConnected:
-        readSerial()
-
-    time.sleep(1)
+    collect_data = {'temperature': temp, 'humidity': humi, 'light':light_intesity}
+    temp += 1
+    humi += 1
+    light_intesity += 1
+    client.publish('v1/devices/me/telemetry', json.dumps(collect_data), 1)
+    time.sleep(5)
