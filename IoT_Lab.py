a233ef4b9949f8bc8ff9f04693629c26133992f9
KSD MSI PC <nhanksd85@gmail.com>
2022-04-12 10:48:12 +0700

    Init for Lab3

--- IoT_Lab.py
+++ IoT_Lab.py
@@ -1,28 +1,63 @@
-print("Xin chào ThingsBoard")
+print("IoT Gateway")
 import paho.mqtt.client as mqttclient
 import time
 import json
+import serial.tools.list_ports
 
 BROKER_ADDRESS = "demo.thingsboard.io"
 PORT = 1883
-THINGS_BOARD_ACCESS_TOKEN = ""
+mess = ""
+
+#TODO: Add your token and your comport
+#Please check the comport in the device manager
+THINGS_BOARD_ACCESS_TOKEN = "9j3nBQSzOHaeLqiku1G4"
+bbc_port = "COM3"
+if len(bbc_port) > 0:
+    ser = serial.Serial(port=bbc_port, baudrate=115200)
+
+def processData(data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    #TODO: Add your source code to publish data to the server
+
+def readSerial():
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
 
 
 def subscribed(client, userdata, mid, granted_qos):
     print("Subscribed...")
 
-
 def recv_message(client, userdata, message):
     print("Received: ", message.payload.decode("utf-8"))
     temp_data = {'value': True}
+    cmd = 1
+    #TODO: Update the cmd to control 2 devices
     try:
         jsonobj = json.loads(message.payload)
-        if jsonobj['method'] == "setValue":
+        if jsonobj['method'] == "setLED":
+            temp_data['value'] = jsonobj['params']
+            client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
+        if jsonobj['method'] == "setFAN":
             temp_data['value'] = jsonobj['params']
             client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
     except:
         pass
 
+    if len(bbc_port) > 0:
+        ser.write((str(cmd) + "#").encode())
 
 def connected(client, usedata, flags, rc):
     if rc == 0:
@@ -42,14 +77,10 @@ client.loop_start()
 client.on_subscribe = subscribed
 client.on_message = recv_message
 
-temp = 30
-humi = 50
-light_intesity = 100
-counter = 0
+
 while True:
-    collect_data = {'temperature': temp, 'humidity': humi, 'light':light_intesity}
-    temp += 1
-    humi += 1
-    light_intesity += 1
-    client.publish('v1/devices/me/telemetry', json.dumps(collect_data), 1)
-    time.sleep(5)
+
+    if len(bbc_port) >  0:
+        readSerial()
+
+    time.sleep(1)
